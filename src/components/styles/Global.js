36f0dbf1a2b22f4d0a440aec050e8b05d931812b
Abdulmalik Muhammad <bawasuru@gmail.com>
2022-04-16 02:16:25 +0100

    Initial commit

---
+++ src/components/styles/Global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0px;
+    margin: 0px;
+    font-family: "Manrope", sans-serif;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+`;
