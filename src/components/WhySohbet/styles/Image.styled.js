4ec31608c1b32952b82ed35dc3fe1a5f0beb8951
Abdulmalik Muhammad <bawasuru@gmail.com>
2022-04-22 00:07:10 +0100

    Added whyshobet components

---
+++ src/components/WhySohbet/styles/Image.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ImageStyled = styled.img`
+  width: 40%;
+  max-width: 100%;
+`;
